07ac0590bc77a692cc0f164b847bfed7d3a3a472
pianxian <pianxian@example.com>
2019-07-23 21:35:28 +0800

    修改了数据库

--- blog/blog-core/src/main/resources/sql/blog_tag.sql
+++ blog/blog-core/src/main/resources/sql/blog_tag.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `blog_tag`;
 CREATE TABLE `blog_tag` (
-  `blog_id` INT(11),
-  `tag_id` INT(11),
+  `blog_id` INT,
+  `tag_id` INT,
   FOREIGN KEY (`blog_id`) REFERENCES `blog` (`id`),
   FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8
