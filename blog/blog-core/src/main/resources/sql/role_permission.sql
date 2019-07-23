de963727b2975f52634620a2a679891441c5e3cc
pianxian <pianxian@example.com>
2019-07-23 21:36:42 +0800

    修改了数据库

--- blog/blog-core/src/main/resources/sql/role_permission.sql
+++ blog/blog-core/src/main/resources/sql/role_permission.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE `role_permission` (
-  `role_id` INT(11),
-  `permission_id` INT(11),
+  `role_id` INT,
+  `permission_id` INT,
   FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8
