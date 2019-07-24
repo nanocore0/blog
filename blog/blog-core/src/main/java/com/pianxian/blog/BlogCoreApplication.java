9c06e8d1c4ae70d195abc8ddce3f4b575b8fbff3
pianxian <pianxian@example.com>
2019-07-24 21:28:00 +0800

    添加搜索模块

--- blog/blog-core/src/main/java/com/pianxian/blog/BlogCoreApplication.java
+++ blog/blog-core/src/main/java/com/pianxian/blog/BlogCoreApplication.java
@@ -3,7 +3,7 @@ package com.pianxian.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class BlogCoreApplication {
 
     public static void main(String[] args) {
