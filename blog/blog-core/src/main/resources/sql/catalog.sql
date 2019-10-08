082defb2c01fee2dbdf639db464438ca5bc672ed
pianxian <pianxian@example.com>
2019-10-08 21:10:38 +0800

    更新

--- blog/blog-core/src/main/resources/sql/catalog.sql
+++ blog/blog-core/src/main/resources/sql/catalog.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `catalog`;
 CREATE TABLE `catalog` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100),
+  `name` VARCHAR(50),
   `user_id` INT,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
