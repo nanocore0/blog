8ab33cb4a69b433febb26a64685461f70e5667c1
pianxian <pianxian@example.com>
2019-10-08 21:10:07 +0800

    更新

--- blog/blog-core/src/main/java/com/pianxian/blog/mapper/BlogTagMapper.java
+++ blog/blog-core/src/main/java/com/pianxian/blog/mapper/BlogTagMapper.java
@@ -4,9 +4,9 @@ import com.pianxian.blog.entity.Tag;
 
 public interface BlogTagMapper {
 
-    int insertATagWithBlog(Tag tag); // 用到 Tag 的 id, blogId
+    int insertATagWithBlog(Tag tag); //用到 Tag 的 id, blogId
 
-    int deleteATagWithBlog(Tag tag); // Tag 需要id, blogId
+    int deleteATagWithBlog(Tag tag); //Tag 需要id, blogId
 
     int deleteTagByBlogId(Integer blogId);
 
